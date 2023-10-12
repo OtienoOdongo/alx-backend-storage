b143757a3e0754b33908ea0482033ae331967b48
OtienoOdongo <otabiznake@gamil.com>
2023-10-12 12:53:14 +0000

    creating another tables that has enum

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+--Writing a sql script that creates a TABLE named users
+--The country attribute utilizes type ENUM
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id),
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
