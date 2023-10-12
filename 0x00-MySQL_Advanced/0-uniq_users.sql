1ff3fba0096dc52a01a26c94e1fe262634132be1
OtienoOdongo <otabiznake@gamil.com>
2023-10-12 09:57:24 +0000

    sql script for creating a table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
---creating a tabled named users
+--creating an sql script for TABLE
+--the name of the table is users
 CREATE TABLE IF NOT EXISTS users(
   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  email VARCHAR(255) UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
 );
