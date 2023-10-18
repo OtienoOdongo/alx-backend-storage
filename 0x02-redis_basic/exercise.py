8f6cf63b4925791bacb06560a1c2f699b8b741b4
OtienoOdongo <otabiznake@gmail.com>
2023-10-18 12:25:06 -0700

    redis excerise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -101,13 +101,69 @@ class Cache:
         return self.get(key, fn=int)
 
     def count_calls(method: Callable) -> Callable:
+        """
+        A decorator that counts the number of times
+        a method is called and stores it in Redis.
+
+        Args: method (Callable):
+        The method to be wrapped and tracked.
+
+        Returns:
+        Callable: The decorated method.
+        """
+        key = method.__qualname__
+
         @wraps(method)
         def wrapper(self, *args, **kwargs):
-            """Incrementing the count for the method"""
-            key = f"{method.__qualname__}_count"
-            count = self._redis.incr(key)
-            """Executing the original method and return its result"""
-            result = method(self, *args, **kwargs)
-            return result
+            """
+            Wrapper function for counting method calls
+            and calling the original method.
 
+            Args:
+            self: The instance of the class.
+            *args: Positional arguments passed to the method.
+            **kwargs: Keyword arguments passed to the method.
+
+            Returns:
+            Any: The result of the original method.
+            """
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
         return wrapper
+
+        def call_history(method: Callable) -> Callable:
+            """
+            A decorator that stores the inputs
+            and outputs of a method in Redis.
+
+            Args:
+            method (Callable):
+            The method to be wrapped and tracked.
+
+            Returns:
+            Callable: The decorated method.
+            """
+            @wraps(method)
+            def wrapper(self, *args, **kwargs):
+                """
+                Wrapper function for storing method
+                inputs and outputs in Redis.
+
+                Args:
+                self: The instance of the class.
+                *args: Positional arguments passed to the method.
+                **kwargs: Keyword arguments passed to the method.
+
+                Returns:
+                Any: The result of the original method.
+                """
+                input_key = method.__qualname__ + ":inputs"
+                output_key = method.__qualname__ + ":outputs"
+
+                output = method(self, *args, **kwargs)
+
+                self._redis.rpush(input_key, str(args))
+                self._redis.rpush(output_key, str(output))
+                return output
+
+            return wrapper
